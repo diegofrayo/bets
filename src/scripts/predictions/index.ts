da261f842f15ed444eb9ef571e3d9275f44ab49f
Diego Rayo <diegofrayo@gmail.com>
2024-09-16 11:53:49 -0500

    :memo: [predictions] update data

--- src/scripts/predictions/index.ts
+++ src/scripts/predictions/index.ts
@@ -4,27 +4,27 @@ import launchAnalysis from "./analysis";
 // npm run script:predictions
 launchAnalysis({
 	// Config 1
-	// config: "SPECIFIC_DATE",
-	// date: "2024-09-14",
-	// enableRemoteAPI: {
-	// 	FIXTURE_MATCHES: true,
-	// 	PLAYED_MATCHES: false,
-	// 	LEAGUE_STANDINGS: false,
-	// },
+	config: "SPECIFIC_DATE",
+	date: "2024-09-16",
+	enableRemoteAPI: {
+		FIXTURE_MATCHES: true,
+		PLAYED_MATCHES: true,
+		LEAGUE_STANDINGS: true,
+	},
 
 	// Config 2
 	// config: "OFFLINE_REBUILDING",
-	// date: "2024-09-14",
-	// previousDays: 47,
+	// date: "2024-09-15",
+	// previousDays: 48,
 	// updateAnalysisStats: true,
 
 	// Config 3
-	config: "LEAGUES_FIXTURES_UPDATE",
-	leaguesFixturesDates: {
-		from: "2024-09-14",
-		to: "2024-09-14",
-		ids: [],
-	},
+	// config: "LEAGUES_FIXTURES_UPDATE",
+	// leaguesFixturesDates: {
+	// 	from: "2024-09-16",
+	// 	to: "2024-09-19",
+	// 	ids: [],
+	// },
 
 	// Config 4
 	// config: "LEAGUES_STANDINGS_UPDATE",
